c641fabe54d8286a4366de306d27bea2be73488a
Artur Nowak <arturmareknowal@gmail.com>
2021-05-27 20:12:41 +0200

    increased mantissa bit representation

--- TaylorSeries.sv
+++ TaylorSeries.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Create Date: 24.04.2021 16:19:42
+// Create Date: 27.05.2021 20:09
 // Design Name: TaylorSeries.sv
 // Module Name: TaylorSeries
 // Project Name: System dedykowany realizaujący aproksymacje szeregu Taylor na platformie FPGA
@@ -11,7 +11,7 @@
 // Dependencies: None
 // 
 // Revision: 
-// Revision 0.02 - Toy example of behavioral
+// Revision 0.03 - increased FXP
 // Additional Comments: none
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -21,24 +21,32 @@ module TaylorSeries(clock, reset, start, ready_out, regAngle, tempAngle );
 
 //Fixed Point
 parameter integer W = 24; 
-parameter FXP_MUL = 1024;
-parameter FXP_SHIFT = 10;
+parameter FXP_MUL = 8388608;
+parameter FXP_SHIFT = 23;
 
 //Input, outputs
 input clock, reset, start;
 input [W-1:0] regAngle;
 output reg ready_out;
-output reg [W-1:0] tempAngle ;
+output reg [W-1:0] tempAngle;
 
 //Taylor coefficients
-reg signed [W-1:0] divider[0:3] = { 12'b000000000001, 12'b000000101010, 12'b001000000000, 12'b000000000001 };
-
+reg signed [W-1:0] divider[0:3] = { 24'b00000000010110110000011, 
+                                    24'b00001010101010101010101, 
+                                    24'b10000000000000000000000, 
+                                    24'b00000000000000000000001 };
+/*
+reg signed [W-1:0] divider[0:3] = { 24'b000000000000000001011011, 
+                                    24'b000000000000101010101011, 
+                                    24'b000000001000000000000000, 
+                                    24'b000000001000000000000000 };
+*/
 //States
 parameter S1 = 4'h00, S2 = 4'h01, S3 = 4'h02, S4 = 4'h03, S5 = 4'h04, S6 = 4'h05, S7 = 4'h06, S8 = 4'h07, S9 = 4'h08, S10 = 4'h09, S11 = 4'h0A;
 reg [2:0] state;
 
 //Temporary variables
-reg signed [W -1:0] const_x2, temp_x2, temp_x4, temp_x6;
+reg signed [2*W:0] const_x2, temp_x2, temp_x4, temp_x6;
 
 always @ (posedge clock)
 begin
@@ -54,37 +62,38 @@ begin
             if(start == 1'b1) state <= S2; else state <= S1;
            end
         S2: begin
-            const_x2 <= (regAngle * regAngle) / FXP_MUL;
+            const_x2 <= (regAngle * regAngle) >> FXP_SHIFT;
             tempAngle  <= 0;
             temp_x2 <= 0;
             temp_x4 <= 0;
             temp_x6 <= 0;
             ready_out <= 0;
             state <= S3;
+            //$display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, regAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, regAngle);
         end
         S3: begin
-            temp_x2 <= (const_x2 * divider[2]) >> 10;
-            temp_x4 <= (const_x2 * divider[1]) >> 10;
-            temp_x6 <= (const_x2 * divider[0]) >> 10;
+            temp_x2 <= (const_x2 * divider[2]) >> FXP_SHIFT;
+            temp_x4 <= (const_x2 * divider[1]) >> FXP_SHIFT;
+            temp_x6 <= (const_x2 * divider[0]) >> FXP_SHIFT;
             state <= S4;
-            $display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
+            //$display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d, div[2] = %d, div[1] = %d, div[0] = ", const_x2, temp_x2, temp_x4, temp_x6, tempAngle, divider[2], divider[1], divider[0]);
         end
         S4:begin
-            temp_x6 <= (const_x2 * temp_x6) >> 10;
-            temp_x4 <= (const_x2 * temp_x4) >> 10;
+            temp_x6 <= (const_x2 * temp_x6) >> FXP_SHIFT;
+            temp_x4 <= (const_x2 * temp_x4) >> FXP_SHIFT;
             state <= S5;
-            $display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
+            //$display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
         end
         S5:begin
-            temp_x6 <= (const_x2 * temp_x6) >> 10;
+            temp_x6 <= (const_x2 * temp_x6) >> FXP_SHIFT;
             state <= S6;
-            $display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
+            //$display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
         end
         S6:begin
             tempAngle  <= 1 * FXP_MUL -  temp_x2 + temp_x4 - temp_x6;  
             ready_out = 1;
             state <= S7;
-            $display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
+            //$display("const_x2 = %d, tempx2 = %d, tempx4 = %d, tempx6 = %d, tempAngle = %d", const_x2, temp_x2, temp_x4, temp_x6, tempAngle);
         end
         S7: begin
             if(start == 1'b0) state <= S7; else state <= S1;
--- TaylorSeries_tb.sv
+++ TaylorSeries_tb.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Create Date: 24.04.2021 16:19:42
+// Create Date: 27.05.2021 20:10
 // Design Name: TaylorSeries.sv
 // Module Name: TaylorSeries
 // Project Name: System dedykowany realizaujący aproksymacje szeregu Taylor na platformie FPGA
@@ -11,22 +11,24 @@
 // Dependencies: None
 // 
 // Revision: 
-// Revision 0.02 - Toy example of behavioral
+// Revision 0.03 - increased FXP
 // Additional Comments: none
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module TaylorSeries_tb;
+parameter integer W = 24; 
 reg clock, reset, start;
-reg [23:0] angle_in;
+reg [W-1:0] angle_in;
 wire ready_out;
-wire [23:0] cos_out;
+wire [W-1:0] cos_out;
 real real_cos;
 
-parameter FXP_MUL = 1024;
+parameter FXP_MUL = 8388608.0;
 
 TaylorSeries TaylorSeries( clock, reset, start, ready_out, angle_in, cos_out);
+
 //Clock generator
 initial
  clock <= 1'b1;
@@ -37,20 +39,24 @@ always
 initial
 begin
     reset <= 1'b1;
-    #1000 reset <= 1'b0;
+    #10 reset <= 1'b0;
 end
+
 //Stimuli signals
 initial
 begin
-    angle_in <= 1.5 * 1024; //Modify value in fixed-point [2:10]
+    angle_in <= 0.5 * FXP_MUL; //Modify value in fixed-point [2:10]
     start <= 1'b0;
-    #2000 start <= 1'b1;
-    #5 start <= 1'b0;
+    #20 start <= 1'b1;
 end
 always @ (posedge ready_out)
 begin
-    #10 real_cos = cos_out;
-    real_cos = real_cos;
-    $display("Real values: cos=%f", real_cos / FXP_MUL);
+    #10 real_cos = cos_out /FXP_MUL;
+    //$display("Real values: cos=%f", real_cos);
+    //#100 angle_in <= angle_in + 0.1 * FXP_MUL; // Increment angle
+      
+    //if (angle_in > 1.5 * FXP_MUL )
+      //$stop; 
+
 end
 endmodule
